69085211ea9b34258886aca5a53ac04430bd2659
Rafał H. <rharryh@gmail.com>
2017-06-19 20:05:35 +0200

    Initial commit. Reading from serial port.

---
+++ SerialPortManager/readme.md
@@ -0,0 +1,2 @@
+Compile with below flag:
+-Djava.library.path=${project_loc:/SerialPortManager}/lib
---
+++ SerialPortManager/src/main/java/com/navigation/serial/Serial.java
@@ -0,0 +1,127 @@
+package main.java.com.navigation.serial;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier; 
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent; 
+import gnu.io.SerialPortEventListener; 
+import java.util.Enumeration;
+
+
+public class Serial implements SerialPortEventListener {
+	private SerialPort serialPort;
+	
+    /**
+     * Ports specified for each OS.
+     */
+	private static final String PORT_NAMES[] = {
+		"/dev/tty.usbserial-A9007UX1", // Mac OS X
+		"/dev/ttyUSB0", // Linux
+		"COM1", // Windows
+	};
+	
+	private BufferedReader input;
+	private OutputStream output;
+	private static final int TIMEOUT = 2000;
+	private static final int DATARATE = 9600;
+
+	/**
+	 * Find port name for host OS, open it and create read and write buffers.
+	 */
+	public void initialize() {
+	    CommPortIdentifier portId = null;
+	    Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
+	
+	    // first, find an instance of serial port as set in PORT_NAMES
+	    while (portEnum.hasMoreElements()) {
+	        CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+		    
+		    System.out.println(currPortId.getName());
+	        for (String portName : PORT_NAMES) {
+	            if (currPortId.getName().equals(portName)) {
+	                portId = currPortId;
+	                break;
+	            }
+	        }
+	    }
+	    if (portId == null) {
+	        System.out.println("Could not find COM port.");
+	        return;
+	    }
+	
+	    try {
+	        serialPort = (SerialPort) portId.open(this.getClass().getName(), TIMEOUT); // open the port with specified timeout
+	        serialPort.setSerialPortParams(DATARATE,
+	                SerialPort.DATABITS_8,
+	                SerialPort.STOPBITS_1,
+	                SerialPort.PARITY_NONE);
+	
+	        // open the streams
+	        input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+	        output = serialPort.getOutputStream();
+	
+	        serialPort.addEventListener(this);
+	        serialPort.notifyOnDataAvailable(true);
+	    } catch (Exception e) {
+	        System.err.println(e.toString());
+	    }
+	}
+
+    /**
+     * Close the port.
+     */
+	public synchronized void close() {
+	    if (serialPort != null) {
+	        serialPort.removeEventListener();
+	        serialPort.close();
+	    }
+	}
+
+	/**
+	 * This event is responsible for reading the buffer every time when
+	 * data is available.
+	 */
+	public synchronized void serialEvent(SerialPortEvent event) {
+	    if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+	        try {
+	            String inputLine = null;
+	            if (input.ready()) {
+	                inputLine = input.readLine();
+	                System.out.println(inputLine);
+	            }
+	
+	        } catch (Exception e) {
+	            System.err.println(e.toString());
+	        }
+	    }
+	    // ignore all the other eventTypes, but the other ones should be considered (see rxtx docs)
+	}
+
+	/**
+	 * Test class
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+	    Serial main = new Serial();
+	    main.initialize();
+	    
+	    // create new thread
+	    Thread t = new Thread() {
+	        public void run() {
+	            // The following line will keep this app alive for 1000 seconds,
+	            // waiting for events to occur and responding to them (printing incoming messages to console).
+	            try {
+	            	Thread.sleep(1000000);
+	            } catch (InterruptedException ie) {
+	            	System.err.println("Thread was interrupted");
+	            }
+	        }
+	    };
+	    t.start();
+	    
+	    System.out.println("Started");
+	}
+}
