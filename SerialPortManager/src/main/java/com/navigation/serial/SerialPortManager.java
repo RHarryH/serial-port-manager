0e0883987025fca186274996ff9caf1d03817368
Rafał H. <rharryh@gmail.com>
2017-07-03 20:15:55 +0200

    Integration with Java Marine API, test class for Java Marine API, test files

--- SerialPortManager/src/main/java/com/navigation/serial/SerialPortManager.java
+++ SerialPortManager/src/main/java/com/navigation/serial/SerialPortManager.java
@@ -1,22 +1,24 @@
 package main.java.com.navigation.serial;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import gnu.io.CommPortIdentifier; 
-import gnu.io.SerialPort;
-import gnu.io.SerialPortEvent; 
-import gnu.io.SerialPortEventListener;
-/*import net.sf.marineapi.nmea.event.SentenceEvent;
-import net.sf.marineapi.nmea.event.SentenceListener;
-import net.sf.marineapi.nmea.io.SentenceReader;*/
-
 import java.util.Enumeration;
 
+import gnu.io.CommPortIdentifier;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+import main.java.com.navigation.GPSData;
+import net.sf.marineapi.nmea.event.SentenceEvent;
+import net.sf.marineapi.nmea.event.SentenceListener;
+import net.sf.marineapi.nmea.io.SentenceReader;
+import net.sf.marineapi.nmea.parser.DataNotAvailableException;
+import net.sf.marineapi.nmea.sentence.GGASentence;
+import net.sf.marineapi.nmea.sentence.SentenceId;
+import net.sf.marineapi.nmea.sentence.VTGSentence;
 
-public class SerialPortManager implements SerialPortEventListener/*, SentenceListener*/ {
+public class SerialPortManager implements SentenceListener {
 	private SerialPort serialPort;
+	private final static GPSData gps = new GPSData();
 	
     /**
      * Ports specified for each OS.
@@ -27,8 +29,7 @@ public class SerialPortManager implements SerialPortEventListener/*, SentenceLis
 		"COM1", // Windows
 	};
 	
-	//private InputStream input;
-	private BufferedReader input;
+	private InputStream input;
 	private OutputStream output;
 	private static final int TIMEOUT = 2000;
 	private static final int DATARATE = 9600;
@@ -36,7 +37,7 @@ public class SerialPortManager implements SerialPortEventListener/*, SentenceLis
 	/**
 	 * Find port name for host OS, open it and create read and write buffers.
 	 */
-	public void initialize() {
+	private void initialize() {
 	    CommPortIdentifier portId = null;
 	    Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
 	
@@ -63,39 +64,48 @@ public class SerialPortManager implements SerialPortEventListener/*, SentenceLis
 	                SerialPort.DATABITS_8,
 	                SerialPort.STOPBITS_1,
 	                SerialPort.PARITY_NONE);
+	        serialPort.enableReceiveThreshold(1);
 	
 	        // open the streams
-	        input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-	        output = serialPort.getOutputStream();
-	
-	        serialPort.addEventListener(this);
-	        serialPort.notifyOnDataAvailable(true);
-	        serialPort.enableReceiveThreshold(1);
-	        
-		    /*input = serialPort.getInputStream();
-		    SentenceReader sr = new SentenceReader(input);
-			sr.addSentenceListener(this);
-			sr.start();*/
+		    input = serialPort.getInputStream();
+		    
+		    SentenceReader reader = new SentenceReader(input);
+			reader.addSentenceListener(this, SentenceId.VTG);
+			reader.addSentenceListener(this, SentenceId.GGA);
+			/*reader.setExceptionListener(new ExceptionListener() {
+				
+				@Override
+				public void onException(Exception e) {
+					if(e instanceof IOException) {
+						System.err.println("Błąd we/wy (najprawdopodobniej pusty stream");
+					}
+				}
+			});*/
+			reader.start();
+	    } catch(PortInUseException e) {
+	    	System.err.println("Port " + portId.getName() + " is in use.");
 	    } catch (Exception e) {
 	        System.err.println(e.toString());
 	    }
+	}
+	
+	/**
+	 * Get data gathered from serial port and decoded by Marine API.
+	 * @return simplified structure for storing most important data
+	 */
+	public GPSData getGps() {
+		return gps;
 	}
 
 	/**
 	 * Close the port.
 	 */
 	public synchronized void close() {
-	    if (serialPort != null) {
-	        serialPort.removeEventListener();
+	    if (serialPort != null)
 	        serialPort.close();
-	    }
 	}
 
-	public SerialPort getSerialPort() {
-		return serialPort;
-	}
-
-	/*@Override
+	@Override
 	public void readingPaused() {
 		System.out.println("-- Paused --");
 	}
@@ -112,27 +122,24 @@ public class SerialPortManager implements SerialPortEventListener/*, SentenceLis
 
 	@Override
 	public void sentenceRead(SentenceEvent event) {
-		System.out.println(event.getSentence());
-	}*/
-
-	/**
-	 * This event is responsible for reading the buffer every time when
-	 * data is available.
-	 */
-	public synchronized void serialEvent(SerialPortEvent event) {
-	    if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-	        try {
-	            String inputLine = null;
-	            if (input.ready()) {
-	                inputLine = input.readLine();
-	                System.out.println(inputLine);
-	            }
-	
-	        } catch (Exception e) {
-	            System.err.println(e.toString());
-	        }
-	    }
-	    // ignore all the other eventTypes, but the other ones should be considered (see rxtx docs)
+		try {
+			switch(event.getSentence().getSentenceId()) {
+			// vector track an Speed over the Ground
+			case "VTG":
+				VTGSentence vtg = (VTGSentence) event.getSentence();
+				
+				gps.setSpeed(vtg.getSpeedKmh());
+				break;
+			// fix information
+			case "GGA":
+				GGASentence gga = (GGASentence) event.getSentence();
+				
+				gps.setPosition(gga.getPosition());
+				break;
+			}
+		}catch(DataNotAvailableException e) {
+			// this exception is ignored, if data isn't available we print nothing
+		}
 	}
 
 	/**
@@ -143,20 +150,6 @@ public class SerialPortManager implements SerialPortEventListener/*, SentenceLis
 	public static void main(String[] args) throws Exception {
 	    SerialPortManager spm = new SerialPortManager();
 	    spm.initialize();
-	    
-	    // create new thread
-	    Thread t = new Thread() {
-	        public void run() {
-	            // The following line will keep this app alive for 1000 seconds,
-	            // waiting for events to occur and responding to them (printing incoming messages to console).
-	            try {
-	            	Thread.sleep(1000000);
-	            } catch (InterruptedException ie) {
-	            	System.err.println("Thread was interrupted");
-	            }
-	        }
-	    };
-	    t.start();
 	    
 	    System.out.println("Started");
 	}
